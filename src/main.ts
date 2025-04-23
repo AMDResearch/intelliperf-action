57d7fbdeffaca5e5085fe2f0ad6eb0e9d91a3c35
Muhammad Awad <MuhammadAbdelghaffar.Awad@amd.com>
2025-04-23 17:18:49 +0000

    Make sure everything exist
    Signed-off-by: Muhammad Awad <MuhammadAbdelghaffar.Awad@amd.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import * as core from '@actions/core';
+import * as fs from 'fs';
 
 type Application = {
     command: string;
-    output_json: string;
+    output_json?: string;
     build_script?: string;
     apptainer_image?: string;
 };
@@ -19,15 +20,30 @@ async function run() {
         const apps: Application[] = JSON.parse(appsJson);
 
         core.info(`Formula: ${formula}`);
-
         core.info("Applications:");
 
         for (const app of apps) {
             core.info(`- Command: ${app.command}`);
-            core.info(`- Output File: ${app.output_json}`);
+            if (app.output_json) {
+                core.info(`- Output File: ${app.output_json}`);
+            }
+
+            if (app.apptainer_image) {
+                core.info(`- Apptainer Image: ${app.apptainer_image}`);
+                if (fs.existsSync(app.apptainer_image)) {
+                    core.info(`  ✅ Found Apptainer image at ${app.apptainer_image}`);
+                } else {
+                    core.warning(`  ⚠️ Apptainer image not found at ${app.apptainer_image}`);
+                }
+            }
+
             if (app.build_script) {
-                core.info(`  Build Script: ${app.build_script}`);
-                core.info(`  Build Script: ${app.apptainer_image}`);
+                core.info(`- Build Script: ${app.build_script}`);
+                if (fs.existsSync(app.build_script)) {
+                    core.info(`  ✅ Found build script at ${app.build_script}`);
+                } else {
+                    core.warning(`  ⚠️ Build script not found at ${app.build_script}`);
+                }
             }
         }
     } catch (err: any) {
